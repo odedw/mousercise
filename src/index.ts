c2f24ae276deb3715481bb92c946cffce55fb3a1
Oded Welgreen <oded.welgreen@gmail.com>
2020-07-04 11:05:43 -0500

    use webpack to bundle

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
+import * as p5 from "p5";
+
 const sketch = (p: p5) => {
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
   };
 
   p.draw = () => {
-    p.background(255);
+    p.background(200);
   };
 };
 
